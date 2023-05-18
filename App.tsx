ea8bbdb16599c2a6fa45398fc7879c654e8ba035
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-17 23:19:12 -0300

    aula 2 - adicionado textos da tela inicial

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground } from 'react-native';
+import { ImageBackground, View, Text } from 'react-native';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 
 import blurBg from './src/assets/bg-blur.png';
 import Stripes from './src/assets/stripes.svg';
+import NLWLogo from './src/assets/slw-spacetime-logo.svg';
 import { styled } from 'nativewind';
 
 // o nativewind só consegue estilizar componentes do proprio react native
@@ -28,10 +29,26 @@ export default function App() {
   return (
     <ImageBackground 
       source={blurBg} 
-      className='bg-gray-900 flex-1 relative'
+      className='bg-gray-900 flex-1 relative px-8'
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className='absolute left-2'/>
+
+      <View className='flex-1 items-center justify-center gap-6'>
+        <NLWLogo />
+
+        <View className='space-y-2'>
+          <Text 
+          className='text-center font-title text-2xl leading-tight text-gray-50'>
+            Sua cápsula do tempo
+          </Text>
+          
+          <Text className='text-center font-body text-base leading-relaxed text-gray-100'>
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+          </Text>
+        </View>
+
+      </View>
 
       <StatusBar style="light" />
     </ImageBackground>
