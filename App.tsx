521eaae9907316613957c453a7994dd8d52772dd
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-17 22:49:34 -0300

    aula 2 - instalada e iniciado uso das fontes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+
 export default function App() {
+  const [ hasLoadedFonts ] = useFonts({
+    Roboto_400Regular, 
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  });
+
+  // se as fontes ainda n carregaram, n mostra nada, 
+  // só mostra as coisas depois delas terem carregado
+  if (!hasLoadedFonts){
+    return null;
+  }
+
   return (
-    <View className='bg-zinc-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 font-bold text-4xl'>Open up App.js to start working on your app!</Text>
+    <View className='bg-gray-900 flex-1 items-center justify-center'>
+      <Text className='font-alt text-gray-50 text-5xl'>
+        Rocketseat
+      </Text>
       <StatusBar style="light" />
     </View>
   );
