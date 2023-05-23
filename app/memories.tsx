78454ea8eac7824dcc0eb844ff59eef8f2219dd0
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-22 23:22:52 -0300

    aula 5 - feito layout da tela de listar memórias

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,4 +1,4 @@
-import { View, TouchableOpacity, ScrollView } from "react-native";
+import { View, TouchableOpacity, ScrollView, Text, Image } from "react-native";
 import NLWLogo from '../src/assets/slw-spacetime-logo.svg';
 import { Link, useRouter } from "expo-router";
 import Icon from '@expo/vector-icons/Feather';
@@ -16,8 +16,8 @@ export default function NewMemory(){
     }
 
     return (
-        <ScrollView className="flex-1 px-8" contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}>
-            <View className="flex-row mt-4 items-center justify-between">
+        <ScrollView className="flex-1" contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}>
+            <View className="flex-row px-8 mt-4 items-center justify-between">
                 <NLWLogo />
 
                 <TouchableOpacity onPress={signOut} className="h-10 w-10 items-center justify-center rounded-full bg-red-500">
@@ -31,6 +31,33 @@ export default function NewMemory(){
                         </TouchableOpacity>
                     </Link>
                 </View>
+            </View>
+
+            <View className="mt-6 space-y-10">
+                <View className="space-y-4">
+                    <View className="flex-row items-center gap-2">
+                        <View className="h-px w-5 bg-gray-50"/>
+                        <Text className="font-body text-xs text-gray-100">12 de abril, 2023</Text>
+                    </View>
+                    <View className="space-y-4 px-8">
+                        <Image 
+                            source={{uri: 'http://10.0.0.220:3333/uploads/f44d36f6-5f9a-4121-8e49-c8175fbf0314.jpg'}} 
+                            className="aspect-video w-full rounded-lg"
+                            alt=""
+                        />
+                        <Text className="font-body text-base leading-relaxed text-gray-100">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor voluptatem culpa cum reiciendis dignissimos fugit, magni sunt aliquam nulla tenetur impedit laborum iusto illo voluptas voluptatum corporis sed, ut explicabo.
+                        </Text>
+                        <Link href="/memories/id" asChild>
+                            <TouchableOpacity className="flex-row items-center gap-2">
+                                <Text className="font-body text=sm text-gray-200">Ler mais</Text>
+                                <Icon name="arrow-right" size={16} color="#8cebb6"/>
+                            </TouchableOpacity>
+                        </Link>
+                    </View>
+
+                </View>
+
             </View>
         </ScrollView>
     )
